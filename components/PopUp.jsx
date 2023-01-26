b97b4f86aecb25f0fa9b94ec979aca81cc96444a
Jordan <frerichs.jordan@gmail.com>
2023-01-26 14:29:15 -0800

    fixed popup styling

--- components/PopUp.jsx
+++ components/PopUp.jsx
@@ -6,7 +6,7 @@ function PopupHowTo() {
     <div className="ml-2">
       <Popup
         trigger={
-          <button className="mt-1 rounded-md outline outline-offset-2 outline-stone-800/50 hover:text-sky-900 hover:outline-sky-900/50 dark:hover:text-green-300 dark:hover:outline-green-300/50 dark:text-stone-200 dark:outline-stone-200/50">
+          <button className="mt-2 rounded-md outline outline-offset-2 outline-stone-800/50 hover:text-sky-900 hover:outline-sky-900/50 dark:hover:text-green-300 dark:hover:outline-green-300/50 dark:text-stone-200 dark:outline-stone-200/50">
             How to play
           </button>
         }
